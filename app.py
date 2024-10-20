fe02ef1bebf8cae5f0958524b0e8d51311c3b437
Eduardo Pizorno <pizorno@gmail.com>
2024-10-20 08:39:23 -0300

    Adicionando rota get_on_task

--- app.py
+++ app.py
@@ -17,13 +17,20 @@ def create_task():
         description=data.get("description", "")
     )
     tasks.append(new_task)
-    return jsonify({"message": "Nova tarefa criada com sucesso!"})
+    return jsonify({"message": "Nova tarefa criada com sucesso!"}),201
 
 @app.route("/tasks", methods=["GET"])
 def get_all_tasks():
     task_list = [task.to_dict() for task in tasks]
     output = {"tasks": task_list, "total_tasks": len(task_list)}
-    return jsonify(output)
+    return jsonify(output),200
+
+@app.route("/tasks/<uuid:id>", methods=["GET"])
+def get_one_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    return jsonify({"message": "Não foi possível encontrar a task"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
